6cf3dc5b2f3c2a69a14d139668d7f956b53fe006
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-10-05 11:10:14 +0900

    Add:グローバルなCSSスタイルを設定 #5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import { Router } from '@/router/Router'
+import './App.css';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme/Theme';
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 export const App = () => {
-  return <Router />
+  return(
+    <ThemeProvider theme={theme}>
+      <Router />
+    <CssBaseline />
+    <div className='App'>
+      <p>テキストの色も変更</p>
+    </div>
+  </ThemeProvider>
+  )
 }
