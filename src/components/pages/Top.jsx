a939f70185f9c5190591235bc74dadb64b74b8de
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-10-09 10:48:59 +0900

    Change:Footerコンポーネント名とfooterのクラス名をTopfooter、topfooterに変更 #16

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,5 +1,5 @@
 import { Header } from '../layouts/Header'
-import { Footer } from '../layouts/Footer'
+import { TopFooter } from '../layouts/TopFooter'
 import './Top.css'
 
 export const Top = () => {
@@ -9,7 +9,7 @@ export const Top = () => {
       <h1 className="title">Movie Connect</h1>
       <p className="subtitle">さあ、映画ライフを楽しもう</p>
       <p className="explainapp">アプリの説明</p>
-      <Footer />
+      <TopFooter />
     </>
   )
 }
