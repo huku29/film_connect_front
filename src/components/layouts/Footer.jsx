9179863803b08d8257c0662a56d7d2fe44b41934
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-10-09 11:19:17 +0900

    Add:フッターナビゲーションを作成 #14

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,9 +1,50 @@
-import './Footer.css'
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import BottomNavigation from '@mui/material/BottomNavigation'
+import BottomNavigationAction from '@mui/material/BottomNavigationAction'
+import SendIcon from '@mui/icons-material/Send'
+import EmailIcon from '@mui/icons-material/Email'
+import MovieIcon from '@mui/icons-material/Movie'
+import { Link} from 'react-router-dom';
 
-export const Footer = () => {
+
+export default function Footer() {
+  const [value, setValue] = React.useState(0)
+
   return (
-    <>
-      <footer className="footer"></footer>
-    </>
+    <Box
+      sx={{
+        width: 1,
+        '& .MuiBottomNavigationAction-root, .Mui-selected, svg': {
+          backgroundColor: 'black',
+          color: '#ff9800',
+        },
+      }}
+    >
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue)
+        }}
+        sx={[
+          {
+            backgroundColor: 'black',
+            height: '50px',
+            color: '#fff',
+            textAlign: 'center',
+            width: 1,
+            position: 'absolute',
+            bottom: 0,
+          },
+        ]}
+      >
+        {/* このLinkタグの書き方がどういう意味なのかまだわかっていない */}
+        <BottomNavigationAction label="送る" icon={<SendIcon />}  component={Link}
+        to={"/"} />
+        <BottomNavigationAction label="受け取る" icon={<EmailIcon />} />
+        <BottomNavigationAction label="鑑賞済み" icon={<MovieIcon />} />
+      </BottomNavigation>
+    </Box>
   )
 }
