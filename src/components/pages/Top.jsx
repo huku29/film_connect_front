b29bb7c916cee9ab7b1f33a86bd8ae2f2eb4dd72
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-10-10 20:58:42 +0900

    Fix:Topコンポーネントの修正 #19

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,15 +1,27 @@
-import { Header } from '../layouts/Header'
-import { TopFooter } from '../layouts/TopFooter'
-import './Top.css'
+import { BaseLayout } from '@/components/layouts'
+import { Stack, Typography } from '@mui/material'
 
-export const Top = () => {
+export const Top = () => { 
   return (
-    <>
-      <Header />
-      <h1 className="title">Movie Connect</h1>
-      <p className="subtitle">さあ、映画ライフを楽しもう</p>
-      <p className="explainapp">アプリの説明</p>
-      <TopFooter />
-    </>
+    <BaseLayout>
+      <Stack
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          textAlign: 'center',
+        }}
+      >
+        <Typography variant="h4">Movie Connect</Typography>
+        <Typography variant="subtitle1">さあ、映画ライフを楽しもう</Typography>
+        <Typography
+          variant="subtitle1"
+          sx={{ padding: '5px', cursor: 'pointer' }}
+        >
+          アプリの説明
+        </Typography>
+      </Stack>
+    </BaseLayout>
   )
 }
