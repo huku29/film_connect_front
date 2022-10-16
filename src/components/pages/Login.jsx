332d07de9fd26110ff13199f6657aa06d29d9bc4
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-10-16 22:26:23 +0900

    Add:ログインページ作成 #25

---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,11 @@
+import { BaseLayout } from '@/components/layouts'
+
+export const Login = () => {
+  return (
+    <>
+      <BaseLayout>
+        <p>Loginコンポーネント</p>
+      </BaseLayout>
+    </>
+  )
+}
