da99c3d3982dde1f222abf7ca16a2a0bc02e80f9
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-10-23 12:41:39 +0900

    Add:ログイン後のヘッダーナビゲーションを作成 #22

--- src/components/layouts/LoggedInHeader.jsx
+++ src/components/layouts/LoggedInHeader.jsx
@@ -1,17 +1,150 @@
-import { Box } from '@mui/material'
+import { useState } from 'react'
+import { styled, useTheme } from '@mui/material/styles'
+import { Box, Drawer } from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline'
+import MuiAppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import List from '@mui/material/List'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import LoginIcon from '@mui/icons-material/Login'
+import { Link } from 'react-router-dom'
 
-export const LoggedInHeader = () => {
+const drawerWidth = 240
+
+
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    //開いた時にMovieConnectという文字がずれるようにする
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}))
+
+//矢印の位置を調整
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}))
+
+
+
+
+//exportしているコンポーネント
+export function LoggedInHeader() {
+  const theme = useTheme()
+  const [open, setOpen] = useState(false)
+
+    /*ドロワーを開けるアニメーション*/
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+    /*ドロワーを閉めるアニメーション*/
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
   return (
-    <Box 
-      component='div' 
-      sx={{
-        backgroundColor: 'black',
-        height: '50px',
-        color: '#fff',
-        textAlign: 'center',
-        width: '100%',
-      }}
-    />
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        open={open}
+        sx={{ backgroundColor: 'black', color: '#ff9800' }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }), right: 0 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Film Connect
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            //ハンバーガメニューを押すと幅が変わる
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        {/*横のスライドの矢印マーク*/}
+        <DrawerHeader>
+          {/* /*矢印を押したら閉まる*/ }
+          <IconButton onClick={handleDrawerClose}>
+          {/* themeのdirectionがltrの場合、左書きのげんとといういみ */}
+            {theme.direction === 'ltr' ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+
+        <Divider />
+
+        <List>
+          {/* <Link to='/show'>
+          {['Login', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? < LoginIcon Link to='/show'/> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+          </Link> */}
+          <Link to="/">
+            <ListItem>
+              <ListItemButton>
+                <ListItemIcon>
+                  <LoginIcon />
+                </ListItemIcon>
+                <ListItemText primary="Logout" sx={{color:'black'}}/>
+              </ListItemButton>
+            </ListItem>
+          </Link>
+        </List>
+      </Drawer>
+    </Box>
   )
 }
 
