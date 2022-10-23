bdebc2581cc327323008806505ce67393b4d4f38
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-10-23 22:08:45 +0900

    Fix:Myページのタブの位置を修正する #27

--- src/components/pages/MyPage.jsx
+++ src/components/pages/MyPage.jsx
@@ -4,15 +4,26 @@ import Box from '@mui/material/Box'
 import Tabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab'
 
-export const Mypage = () => {
-  const [value, setValue] = useState(1)
+export const MyPage = () => {
+  const [value, setValue] = useState(0)
   const handleChange = (event, newValue) => {
     setValue(newValue)
   }
   return (
     <>
       <LoggedInLayout>
-        <Box sx={{ width: '100%', bgcolor: 'background.default'}}>
+        <Box
+          sx={{
+            width: '100%',
+            bgcolor: 'background.default',
+            position: 'absolute',
+            top: '30%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            textAlign: 'center',
+            border: 'balck',
+          }}
+        >
           <Tabs value={value} onChange={handleChange} centered>
             <Tab label="送信済みレター" />
             <Tab label="鑑賞済み映画" />
