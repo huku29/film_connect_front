a1d90720d54c2ff009194c0e96d1f6b7d42973f6
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-12-28 10:27:03 +0900

    Add:状態管理する値を追加 #63

--- src/jotai/atoms.js
+++ src/jotai/atoms.js
@@ -4,8 +4,17 @@ import { atom } from 'jotai'
 export const recieveMovieDataAtom = atom({
   movieTitle: '',
   movieImg: '',
+  receiverId: '',
+  letterId: '',
+  userId: '',
   recommendPoint: '',
   twitterUserName: '',
 })
 
 export const handleFadeModal = atom(false)
+
+export const madeLettersDataAtom = atom([])
+
+export const handleFlashMessageAtom = atom(false)
+
+export const handleGetErrorMessageAtom = atom('')
