719304c4afb28724cd21ce6b944253623041193d
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-12-28 09:54:42 +0900

    Add:一覧画面に表示させるためのpathを追加 #63

--- src/urls/index.js
+++ src/urls/index.js
@@ -2,6 +2,11 @@ export const filmsSearch = `${import.meta.env.VITE_DEFAULT_API_URL}/api/v1/films
 export const loginCheck = `${import.meta.env.VITE_DEFAULT_API_URL}/api/v1/users`;
 export const sendLetter = `${import.meta.env.VITE_DEFAULT_API_URL}/api/v1/letters`;
 export const getLetter = `${import.meta.env.VITE_DEFAULT_API_URL}/api/v1/letters/random`;
+export const getMadeLetters = `${import.meta.env.VITE_DEFAULT_API_URL}/api/v1/letters`;
+export const getFilmsDetails = `${import.meta.env.VITE_DEFAULT_API_URL}/api/v1/films/info`;
+export const registerReceivedLetter = `${import.meta.env.VITE_DEFAULT_API_URL}/api/v1/received_letters`;
+export const getReceivedLetters = `${import.meta.env.VITE_DEFAULT_API_URL}/api/v1/received_letters`;
+export const getReceivedLettersData = `${import.meta.env.VITE_DEFAULT_API_URL}/api/v1/received_letters/info`;
 
 export const filmsimg = `${import.meta.env.VITE_FILM_IMG_URL}`;
 export const filmsImgSmall = `${import.meta.env.VITE_FILM_IMG_URL_SMALL}`;
